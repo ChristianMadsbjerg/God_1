2462a59edde359220bdf37197c9f434c53b625b2
ChristianMadsbjerg <91522732+ChristianMadsbjerg@users.noreply.github.com>
2023-09-26 12:07:20 +0200

    Update DetailsScreen.js

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,38 +1,29 @@
-import {Button, StyleSheet, Text, View} from "react-native";
-import * as React from "react";
+// Importerer nødvendige moduler fra React og React Native bibliotekerne.
+import {Button, StyleSheet, Text, View} from "react-native"; // Importerer komponenter fra React Native biblioteket.
+import * as React from "react"; // Importerer React biblioteket for at kunne bruge JSX og React komponenter.
 
-/*
-* Metode til at navigere på baggrund af de argumenter, som der sendes med i metode
-* Metodens logik udnytter den prædefinerede metode, 'navigate', som navigere ind til det den komponent,
-* der hænger sammen med det overførte rutenavn
- */
-const navController = (navigation, route) =>{
-    navigation.navigate(route)
+// Definerer en hjælpefunktion 'navController', der navigerer til en given rute.
+const navController = (navigation, route) =>{ 
+    navigation.navigate(route) // Bruger 'navigate' metoden fra 'navigation' objektet til at navigere til den specificerede rute.
 }
 
-
-/*
- * DetailsScreen tager 'navigation' med som argument. navigation er en automatisk prædefineret prop, der kan refereres til i alle screen komponenter.
- *Se dokumentationen for mere info: https://reactnavigation.org/docs/navigation-prop/
- *
- *Derudover styles DetailsScreen, som indeholder omfatter to button komponenter, der benytters til at aktivere vores navController metode
- */
-function DetailsScreen({navigation}) {
+// Definerer en React komponent ved navn 'DetailsScreen'.
+function DetailsScreen({navigation}) { 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>DetailsScreen!</Text>
-            <Button title="Go To Screen One" onPress={() => navController(navigation, 'ScreenOne')}  />
-            <Button title="Go To Screen Two" onPress={() => navController(navigation, 'ScreenTwo')}  />
+        <View style={styles.container}> // Bruger en 'View' komponent med en bestemt stil.
+            <Text style={styles.text}>DetailsScreen!</Text> // Viser en tekst med en bestemt stil.
+            <Button title="Go To Screen One" onPress={() => navController(navigation, 'ScreenOne')}  /> // Tilføjer en knap, der navigerer til 'ScreenOne' når den trykkes på.
+            <Button title="Go To Screen Two" onPress={() => navController(navigation, 'ScreenTwo')}  /> // Tilføjer en knap, der navigerer til 'ScreenTwo' når den trykkes på.
         </View>
     );
 }
 
-export default DetailsScreen
+// Eksporterer 'DetailsScreen' komponenten, så den kan bruges i andre filer.
+export default DetailsScreen 
 
-
-//Lokal styling til brug i DetailsScreen.
-const styles = StyleSheet.create({
-    container: {
+// Definerer en række stilarter ved hjælp af React Native's 'StyleSheet' metode.
+const styles = StyleSheet.create({ 
+    container: { // Stil for containeren.
         paddingTop:100,
         paddingBottom:100,
         borderColor: 'green',
@@ -43,7 +34,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         height:'100%'
     },
-    text: {
+    text: { // Stil for teksten.
         fontSize: 20,
     },
 });
